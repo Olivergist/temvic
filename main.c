7a7da309a968d5cb3167a0a58019df192c5a25de
Twin J <junohanyere@gmail.com>
2023-09-16 11:03:33 +0100

    creating new process

--- main.c
+++ main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 /**
- *  * main - Command line Interpreter
- *   * Return: 0
- *    */
+ * main - Command line Interpreter
+ * Return: 0
+ */
 int main(int argc, char *argv[], char **env)
 {
 	ssize_t status;
+	int i;
 	char *lineptr = NULL;
 	size_t n = sizeof(size_t) * 1024;
 	char *args[] = {"/bin/ls", NULL};
+	pid_t child;
 
 	while (argc)
 	{
@@ -21,10 +24,20 @@ int main(int argc, char *argv[], char **env)
 		if (status == -1)
 			exit(0);
 		lineptr[strlen(lineptr) - 1] = '\0';
-		if (execve(lineptr, args, env) == -1)
-			perror(argv[0]); 
+		child = fork();
+		if (child == 0)
+		{
+			if (execve(lineptr, args, env) == -1)
+				perror(argv[0]);
+			exit(0);
+		}
+		else
+		{
+			do {
+				waitpid(child, &i, WUNTRACED);
+			} while (!WIFEXITED(i) && !WIFSIGNALED(i));
+		}
 	}
 	free(lineptr);
 	return (0);
 }
-~   
